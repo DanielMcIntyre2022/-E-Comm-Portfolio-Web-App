a375cac695caa0781101e1bb7e8a6f6ecb25a8f2
Daniel <daniel-mcintyre@hotmail.com>
2022-12-05 13:52:53 -0500

    updates to slider component and created sliderItems data set file

---
+++ frontend/src/data.js
@@ -0,0 +1,16 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=495&q=80',
+        title: 'FALL SALE',
+        description: 'GET FALL READY WITH STYLE! GET 30% OFF ALL FALL JACKETS',
+        bg: 'bg-slate-50'
+    },
+    {
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1632149877166-f75d49000351?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+        title: 'POPULAR ITEMS',
+        description: 'DO NOT MISS OUT ON THESE POPULAR ITEMS!',
+        bg: 'bg-slate-50'
+    }
+]
