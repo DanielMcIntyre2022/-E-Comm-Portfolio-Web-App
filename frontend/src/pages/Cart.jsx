fe77cf7127fa53090017d7d89057530f2ca7fad2
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-07 18:15:19 -0500

    additional updates to cart page component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -41,10 +41,44 @@ function Cart() {
                                 </div>
                             </div>
                         </div>
+                        <hr className="mt-5 mb-5"/>
+                        <div className="product flex justify-between">
+                            <div className="product-detail flex flex-2 ml-10">
+                                <img className="w-20%" src="https://images.unsplash.com/photo-1611911813383-67769b37a149?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"></img>
+                            <div className="details p-20 flex flex-col">
+                                <h1 className="mb-2"><b className="mr-2">Product:</b>WINTER SWEATER</h1>
+                                <h1 className="mb-2"><b className="mr-2">ID:</b>24304505</h1>
+                                <h1><b className="mr-2">Size:</b>M</h1>
+                                 </div>
+                            </div>
+                            <div className="price-detail flex items-center justify-center flex-col flex-1">
+                                <div className="product-amount-container flex items-center">
+                                    <AddIcon/>
+                                    <p className="m-5 text-2xl">2</p>
+                                    <RemoveIcon/>
+                                </div>
+                                <div className="price">
+                                    <p className="text-2xl">$200</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="product-summary-container">
-                        summary
+                    <div className="product-summary-container flex-1 border p-10">
+                        ORDER SUMMARY
+                    <div className="summary-item mt-5 mb-5 flex justify-between">
+                        <span className="summary-total mr-2">Subtotal:</span>
+                        <span className="summary-price">$400</span>
                     </div>
+                    <div className="summary-item mt-5 mb-5 flex justify-between">
+                        <span className="summary-shipping mr-2">Shipping:</span>
+                        <span className="summary-price">$4.99</span>
+                    </div>
+                    <div className="summary-total mt-5 mb-5 flex justify-between">
+                        <span className="summary-shipping font-bold mr-2">Total:</span>
+                        <span className="summary-price">$404.99</span>
+                    </div>
+                    <button className="border p-2">CHECK OUT NOW</button>
+                </div>
                 </div>
             </div>
         <Footer/>
