ace1f1c3e691278d83d7e9bb951c9081d0f35268
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-08 14:11:47 -0500

    created responsive design for Product page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import Home from './pages/Home';
 import Login from "./pages/Login";
 import Register from './pages/Register'
 import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
-    <ProductList/>
+    <Product/>
     </div>
   );
 }
