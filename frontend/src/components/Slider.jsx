1758db94603a1810ad310b277d5663ab06649409
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-08 12:47:13 -0500

    added responsive design for slider component

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -11,7 +11,7 @@ function Slider() {
     };
 
   return (
-    <div className="slider-container w-100% h-screen flex relative mt-10 bg-white overflow-hidden">
+    <div className="slider-container w-100% h-screen flex relative mt-10 bg-white overflow-hidden max-sm:hidden">
         <div className="arrow-left-container w-50 h-10 bg-slate-100 rounded-full flex items-center justify-center absolute top-0 bottom-0 m-auto left-10 cursor-pointer opacity-50 z-index:2" onClick={()=>handleClick("left")}>
             <ArrowLeftIcon/>
         </div>
