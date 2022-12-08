48376972b19d1f8f2280bf675b3cccc134972f52
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-08 13:48:35 -0500

    created responsive design for login and register page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Cart from "./pages/Cart";
 import Home from './pages/Home';
+import Login from "./pages/Login";
+import Register from './pages/Register'
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <Register/>
     </div>
   );
 }
