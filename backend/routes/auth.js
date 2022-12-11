ab51179dfdff0630b4f2a3dd85f6ed6cf2a027fc
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-11 12:47:32 -0500

    implemented jsonwebtoken in login router function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Cryptojs = require('crypto-js');
-const cryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // REGISTER //
 router.post('/register', async (req, res) => {
@@ -32,10 +32,18 @@ router.post('/login', async (req, res) => {
 
         Orginalpassword !== req.body.password && 
             res.status(401).json('wrong credentials!');
+
+            const accessToken = jwt.sign({
+                id: user._id, 
+                isAdmin: user.isAdmin
+            },
+                process.env.JWT_SEC,
+                {expiresIn:'3d'}
+            );
         
         const { password, ...others} = user._doc;
         
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
 
     } catch (error) {
         res.status(500).json(error);
