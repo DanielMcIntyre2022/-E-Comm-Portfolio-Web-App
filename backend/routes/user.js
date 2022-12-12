0f9ca968b41c85a330d82e21365d08186a5d6339
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-12 11:21:25 -0500

    created a function to get all users in user route file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -41,5 +41,16 @@ router.get('/find/:id', verifyTokenAndAdmin, async(req, res) => {
     }
 });
 
+// GET ALL USERS //
+router.get('/', verifyTokenAndAdmin, async(req, res) => {
+    const query = req.query.new
+    try {
+        const users = query? await User.find().sort({_id:-1}).limit(5) : await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
