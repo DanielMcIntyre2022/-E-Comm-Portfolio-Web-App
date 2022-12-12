db94aa70f3dab8274160e22eb9f3c38b0989bcc1
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-12 15:40:34 -0500

    created an add products post method function in product route file

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('DB Connection Successful'))
@@ -16,7 +17,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
-app.use("/api/users", userRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
 
 app.listen(PORT, () => {
     console.log(`server is running! on port ${PORT}`)
