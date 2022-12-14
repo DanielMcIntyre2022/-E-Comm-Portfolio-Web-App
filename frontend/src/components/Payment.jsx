4cd16a141c2101670ca4ffae6057bce8424d7db3
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-13 19:03:33 -0500

    created useEffect to make client request to send stripe data to backend

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 function Payment() {
@@ -11,6 +13,22 @@ function Payment() {
         setStripeToken(token)
         console.log(token)
     };
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+               const response = await axios.post('http://localhost:3048/api/checkout/payment', {
+                tokenId:stripeToken.id,
+                amount:2000,
+               }
+            );
+            console.log(response.data);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        stripeToken && makeRequest();
+    },[stripeToken]);
 
   return (
     <div className='checkout-container'>
