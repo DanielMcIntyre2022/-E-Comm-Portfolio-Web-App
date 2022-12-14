8635042f417e7cc4c1b41d9a9c4172f39f3d2f0e
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-14 14:24:28 -0500

    successfully fetched product data from product endpoint from Products component

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,7 +1,25 @@
 import { popularProducts } from '../data';
+import { useState, useEffect } from 'react';
 import Product from './Product';
+import axios from 'axios';
 
-function Products() {
+function Products({catergoryLink, filters, sort}) {
+  
+  const [ products, useProducts ] = useState([]);
+  const [ filteredProducts, setFilteredProducts ] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async() => {
+      try {
+        const response = await axios.get('http://localhost:3048/api/products');
+        console.log(response);
+      } catch (error) {
+        
+      }
+    };
+    getProducts();
+  },[catergoryLink]);
+
   return (
     <div className='products-container p-20 flex justify-between flex-wrap'>
         {popularProducts.map(item => (
