93851f57cc62bd9617f1325f566c428718ad9dd1
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-22 13:17:19 -0500

    created a data file to hold the user data and passed down as props to Chart component

--- admin/src/components/chart/Chart.jsx
+++ admin/src/components/chart/Chart.jsx
@@ -1,67 +1,16 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-function Chart() {
-
-    const data = [
-        {
-          name: 'Jan',
-          "Active User": 4000,
-        },
-        {
-          name: 'Feb',
-          "Active User": 3000,
-        },
-        {
-          name: 'Mar',
-          "Active User": 2000,
-        },
-        {
-          name: 'Apr',
-          "Active User": 2780,
-        },
-        {
-          name: 'May',
-          "Active User": 1890,
-        },
-        {
-          name: 'Jun',
-          "Active User": 2390,
-        },
-        {
-          name: 'Jul',
-          "Active User": 3490,
-        },
-        {
-            name: 'Aug',
-            "Active User": 3490,
-        },
-        {
-            name: 'Sep',
-            "Active User": 6000,
-        },
-        {
-            name: 'Oct',
-            "Active User": 2000,
-        },
-        {
-            name: 'Nov',
-            "Active User": 500,
-        },
-        {
-            name: 'Dec',
-            "Active User": 3490,
-        },
-      ];
+function Chart({title, data, dataKey, grid}) {
       
   return (
     <div className="chart-container m-10 p-10 shadow-xl">
-        <h3 className="chart-title font-semibold mb-10">User Analytics</h3>
+        <h3 className="chart-title font-semibold mb-10">{title}</h3>
         <ResponsiveContainer className="w-100% aspect-[3/1]">
             <LineChart data={data}>
                 <XAxis dataKey="name" stroke="#5550bd"/>
-                <Line type="monotone" dataKey="Active User" stroke="#5550bd"/>
+                <Line type="monotone" dataKey={dataKey} stroke="#5550bd"/>
                 <Tooltip/>
-                <CartesianGrid stroke="#e0dfdf" strokeDasharray='5 5'/>
+                {grid &&  <CartesianGrid stroke="#e0dfdf" strokeDasharray='5 5'/>}
             </LineChart>
         </ResponsiveContainer>
     </div>
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import Chart from "../../components/chart/Chart";
+import { userData } from '../../data';
 
 function Home() {
   return (
     <div className="flex-[4_4_0%]">
         <FeaturedInfo/>
-        <Chart/>
+        <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   )
 }
