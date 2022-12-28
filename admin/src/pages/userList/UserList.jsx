aeec9280374b70b8112ba09baa2402a757e1f456
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-28 13:59:39 -0500

    updated UserList component to display user image

--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -4,7 +4,14 @@ function UserList() {
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 90 },
-        { field: 'username', headerName: 'Username', width: 200 },
+        { field: 'user', headerName: 'User', width: 200, renderCell: (params)=> {
+            return (
+                <div className='userListUser flex items-center'>
+                    <img className='userListImage w-8 h-8 rounded-full object-cover mr-10' src={params.row.avatar} alt='userProfilePic'/>
+                    {params.row.username}
+                </div>
+            )
+        }},
         { field: 'email', headerName: 'Email', width: 200 },
         { field: 'status', headerName: 'Status', width: 120 },
         { field: 'transaction',headerName: 'Transaction Total', width: 160 },
