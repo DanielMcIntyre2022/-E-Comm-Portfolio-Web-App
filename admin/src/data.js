2a3cb79c44ab709d4b519e96ed9542f4a74ea86d
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-29 17:57:41 -0500

    completion of ProductList component

--- admin/src/data.js
+++ admin/src/data.js
@@ -76,3 +76,31 @@ export const userData = [
     },
 
 ];
+
+export const productRows = [
+  { 
+      id: 1, 
+      name: 'Winter Sweater', 
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+      stock: 123,
+      status: 'active',
+      price: '$120'
+  },
+  { 
+      id: 2, 
+      name: 'Winter Sweater', 
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+      stock: 123,
+      status: 'active',
+      price: '$120'
+  },
+  { 
+      id: 3, 
+      name: 'Winter Sweater', 
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+      stock: 123,
+      status: 'active',
+      price: '$120'
+  },
+
+];
--- admin/src/pages/productList/ProductList.jsx
+++ admin/src/pages/productList/ProductList.jsx
@@ -1,8 +1,51 @@
+import { DataGrid } from '@mui/x-data-grid';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { productRows } from '../../data';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function ProductList() {
+
+    const [data, setData] = useState(productRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter(item=>item.id !== id));
+    };
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'product', headerName: 'Product', width: 200, renderCell: (params)=> {
+            return (
+                <div className='productListUser flex items-center'>
+                    <img className='productListImage w-8 h-8 rounded-full object-cover mr-10' src={params.row.img} alt='userProfilePic'/>
+                    {params.row.name}
+                </div>
+            )
+        }},
+        { field: 'stock', headerName: 'Stock', width: 200 },
+        { field: 'status', headerName: 'Status', width: 120 },
+        { field: 'price',headerName: 'Price', width: 160 },
+        { field: 'action', headerName: 'Action', width: 150, renderCell: (params) => {
+            return (
+                <>
+                <Link to={'/product/' + params.row.id}>
+                    <button className='product-list-edit mr-10 cursor-pointer'>Edit</button>
+                </Link>
+                    <DeleteOutlineIcon className='product-list-delete cursor-pointer text-red-500' onClick={() => handleDelete(params.row.id)}/>
+                </>
+            )
+        }}
+      ];
+
   return (
     <div className="product-list flex-[4]">
-        ProductList
+        <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        checkboxSelection
+        pageSize={10}
+      />
     </div>
   )
 };
