4cd425d6ccce8602ae706ea454b281177384fd2a
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2022-12-30 16:36:09 -0500

    updates to NewProduct component

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,6 +7,7 @@ import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -23,7 +24,7 @@ function App() {
             {/* products */}
             <Route path='/products' element={<ProductList/>}/>
             <Route path='/product/:productId' element={<Product/>}/>
-            <Route path='/newProduct' element={<NewUser/>}/>
+            <Route path='/newProduct' element={<NewProduct/>}/>
           </Routes>
       </div>
     </Router>
