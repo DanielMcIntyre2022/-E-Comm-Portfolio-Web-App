df7913f51b27adc2ee52148a3f94ac6b2c466c98
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-03 13:22:58 -0500

    added dispatch login function to Login component and created login route

--- admin/src/App.js
+++ admin/src/App.js
@@ -8,6 +8,7 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
@@ -25,6 +26,8 @@ function App() {
             <Route path='/products' element={<ProductList/>}/>
             <Route path='/product/:productId' element={<Product/>}/>
             <Route path='/newProduct' element={<NewProduct/>}/>
+            {/* login */}
+            <Route path='/login' element={<Login/>}/>
           </Routes>
       </div>
     </Router>
