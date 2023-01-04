43ed785b7f48a437039dea3376fdf30bfa30a78d
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-04 12:32:20 -0500

    updates/edits to column array in ProductList component

--- admin/src/pages/productList/ProductList.jsx
+++ admin/src/pages/productList/ProductList.jsx
@@ -23,17 +23,16 @@ function ProductList() {
     };
 
     const columns = [
-        { field: '_id', headerName: 'ID', width: 90 },
+        { field: '_id', headerName: 'ID', width: 220 },
         { field: 'product', headerName: 'Product', width: 200, renderCell: (params)=> {
             return (
                 <div className='productListUser flex items-center'>
                     <img className='productListImage w-8 h-8 rounded-full object-cover mr-10' src={params.row.img} alt='userProfilePic'/>
-                    {params.row.name}
+                    {params.row.title}
                 </div>
             )
         }},
-        { field: 'stock', headerName: 'Stock', width: 200 },
-        { field: 'status', headerName: 'Status', width: 120 },
+        { field: 'inStock', headerName: 'Stock', width: 200 },
         { field: 'price',headerName: 'Price', width: 160 },
         { field: 'action', headerName: 'Action', width: 150, renderCell: (params) => {
             return (
